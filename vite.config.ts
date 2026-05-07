5fc81d69219f4494f889bd3d6f7aa0c347dabb7e
Wexenia <86799889+Wexenia@users.noreply.github.com>
2026-05-07 15:24:50 +0200

    Update vite.config.ts
    or not

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
   
-  base: '/wexenia/',
+  base: '/stipendie-kompassen/',
   server: {
     host: "::",
     port: 8080,
