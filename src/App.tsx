b982532ee398a9eaed23aecd7189eadcdbc5c594
Wexenia <86799889+Wexenia@users.noreply.github.com>
2026-05-07 16:52:57 +0200

    Update App.tsx
    Should solve the 404 issues

--- src/App.tsx
+++ src/App.tsx
@@ -40,7 +40,7 @@ const App = () => (
             <Route path="*" element={<NotFound />} />
           </Routes>
         </Layout>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
